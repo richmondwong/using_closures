7301192c805207e07e074984bac5ac1fb5a307d8
Richmond Wong <24490829+richmondwong@users.noreply.github.com>
2019-01-06 17:12:13 -0500

    Fixed indentation

--- using_closures2.js
+++ using_closures2.js
@@ -1,32 +1,26 @@
 var countdownGenerator = function (x) {
-var statements = [
-  "T-minus",
-  "Blast Off!",
-  "Rockets already gone, bub!"
-]
-
-counter = x
+  
+  var statements = [
+    "T-minus",
+    "Blast Off!",
+    "Rockets already gone, bub!"
+  ]
+  
+  var counter = x
 
   return function(){
-
-
     if (counter === 0){
       console.log(statements[1])
       counter -= 1
     }
-
     else  if (counter <= -1){
         console.log(statements[2])
       }
-
       else {
         console.log(statements[0] + " " + counter + "...")
     counter -= 1
-
       }
-
   }
-
 };
 
 var countdown = countdownGenerator(3);
